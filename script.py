889b656e6537e6cea4fc8983ce3dad5565a608ad
Denuwan Yasodhana <denuwanyasodhana123@gmail.com>
2026-03-17 17:00:53 +0530

    Update script.py

--- script.py
+++ script.py
@@ -11,3 +11,16 @@ while True:
     print(response.text)
   except KeyboardInterrupt:
     print("\n Exiting")
+
+
+# For streaming change as
+try:
+    response = client.models.generate_content_stream(
+      model = "____",
+      contents = input("\nEnter your prompt: ")
+    )
+    for chunk in response:
+      if chunk.text:
+        print(chunk.text, end="", flush=True)
+  except KeyboardInterrupt:
+    print("\n Exiting")
